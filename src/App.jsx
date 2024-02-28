f21b4ffdb87b8dcd1469ba5452420db937ba655d
Filip0022 <142938090+Filip0022@users.noreply.github.com>
2024-02-28 18:53:54 +0100

    Some Changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./app.scss";
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
 import Parallax from "./components/parallax/Parallax";
+import Portfolio from "./components/portfolio/Portfolio";
 import Services from "./components/services/Services";
 import Portfolio from "./components/portfolio/Portfolio";
 
