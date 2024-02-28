1a0b75946cbaf34a1b0aa7a3d55d2087b361e8d9
Filip0022 <142938090+Filip0022@users.noreply.github.com>
2024-02-28 18:43:36 +0100

    Add Portfolio Cards

---
+++ src/components/portfolio/Portfolio.jsx
@@ -0,0 +1,34 @@
+import "./portfolio.scss";
+
+const items = [
+  {
+    id: 1,
+    title: "Magic Pixel Paradise",
+    img: "https://images.pexels.com/photos/46148/aircraft-jet-landing-cloud-46148.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis reiciendis saepe quod optio iure maxime facere est impedit. Impedit, perspiciatis?",
+  },
+  {
+    id: 2,
+    title: "Atum-namioty",
+    img: "https://images.pexels.com/photos/46148/aircraft-jet-landing-cloud-46148.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis reiciendis saepe quod optio iure maxime facere est impedit. Impedit, perspiciatis?",
+  },
+  {
+    id: 3,
+    title: "Fit-Club",
+    img: "https://images.pexels.com/photos/46148/aircraft-jet-landing-cloud-46148.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis reiciendis saepe quod optio iure maxime facere est impedit. Impedit, perspiciatis?",
+  },
+  {
+    id: 4,
+    title: "Future Bank App",
+    img: "https://images.pexels.com/photos/46148/aircraft-jet-landing-cloud-46148.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis reiciendis saepe quod optio iure maxime facere est impedit. Impedit, perspiciatis?",
+  },
+];
+
+const Portfolio = () => {
+  return <div className="portfolio"></div>;
+};
+
+export default Portfolio;
