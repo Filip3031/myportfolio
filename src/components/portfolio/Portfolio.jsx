52cc958aa93d5ae738ad9bcc9f554397bfbda7e7
Filip0022 <142938090+Filip0022@users.noreply.github.com>
2024-02-28 18:43:46 +0100

    Some Changes

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,34 +1,87 @@
+import { useRef } from "react";
 import "./portfolio.scss";
+import { motion, useScroll, useSpring } from "framer-motion";
+import { useTransform } from "framer-motion";
 
 const items = [
   {
     id: 1,
     title: "Magic Pixel Paradise",
-    img: "https://images.pexels.com/photos/46148/aircraft-jet-landing-cloud-46148.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    img: "../public/magicpixel.png",
     desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis reiciendis saepe quod optio iure maxime facere est impedit. Impedit, perspiciatis?",
   },
   {
     id: 2,
     title: "Atum-namioty",
-    img: "https://images.pexels.com/photos/46148/aircraft-jet-landing-cloud-46148.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    img: "../public/atum.png",
     desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis reiciendis saepe quod optio iure maxime facere est impedit. Impedit, perspiciatis?",
   },
   {
     id: 3,
     title: "Fit-Club",
-    img: "https://images.pexels.com/photos/46148/aircraft-jet-landing-cloud-46148.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    img: "../public/fitclub.png",
     desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis reiciendis saepe quod optio iure maxime facere est impedit. Impedit, perspiciatis?",
   },
   {
     id: 4,
     title: "Future Bank App",
-    img: "https://images.pexels.com/photos/46148/aircraft-jet-landing-cloud-46148.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    img: "../public/bankist.png",
     desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis reiciendis saepe quod optio iure maxime facere est impedit. Impedit, perspiciatis?",
   },
 ];
 
+const Single = ({ item }) => {
+  const ref = useRef();
+
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    // offset: ["start start", "end start"],
+  });
+
+  const y = useTransform(scrollYProgress, [0, 1], [-300, 300]);
+
+  return (
+    <section>
+      <div className="container">
+        <div className="wrapper">
+          <div className="imageContainer" ref={ref}>
+            <img src={item.img} alt="" />
+          </div>
+          <motion.div className="textContainer" style={{ y }}>
+            <h2>{item.title}</h2>
+            <p>{item.desc}</p>
+            <button>See Demo</button>
+          </motion.div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
 const Portfolio = () => {
-  return <div className="portfolio"></div>;
+  const ref = useRef();
+
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["end end", "start start"],
+  });
+
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 200,
+    damping: 15,
+  });
+
+  return (
+    <div className="portfolio" ref={ref}>
+      <div className="progress">
+        <h1>Featured Works</h1>
+        <motion.div className="progressBar" style={{ scaleX }}></motion.div>
+      </div>
+      {items.map((item) => (
+        <Single item={item} key={item.id} />
+      ))}
+    </div>
+  );
 };
 
 export default Portfolio;
